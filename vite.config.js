76a512b5c2405799706facb54ea42d1616e1a199
hfgong <haifeng.gong@outlook.com>
2025-06-16 18:13:29 -0700

    deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/hanzi/', // 这里请确保与你的仓库名一致
   plugins: [vue()],
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3001
+    outDir: 'dist'
   }
 });
